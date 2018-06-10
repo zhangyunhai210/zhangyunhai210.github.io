940287216198e0409f24b8387dffdeddd2269d18
Zhang <zhangyunhai210@gmail.com>
2018-06-09 23:20:53 -0700

    2018-06-09

--- gitpush.sh
+++ gitpush.sh
@@ -15,7 +15,7 @@ fi
 echo -e "\033[32m---------->>> push end\033[0m\n"
 echo -e "\033[32m----------------------------------------------------\033[0m\n"
 hexo g -d
-if[ $? -eq 0 ];then
+if [ $? -eq 0 ];then
     echo -e "\033[32m---------->>> hexo push succeed !!!\033[0m\n"
 else
     echo -e "\033[32m---------->>> hexo push failed !!!\033[0m\n"
