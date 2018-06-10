deddf649ca254b7197999d7ab51434899ef0ae1f
Zhang <zhangyunhai210@gmail.com>
2018-06-09 23:03:45 -0700

    2018-06-09

---
+++ gitpush.sh
@@ -0,0 +1,16 @@
+#! /bin/zsh
+DATE=`date +"%Y-%m-%d"`
+
+echo "Start git,please wait...."
+git add .
+echo "add end..."
+git commit -m "${DATE}"
+echo "commit end..."
+git push origin hexo
+if[$? -eq 0] then
+    echo -e "\033[32m---------->>>  push succeed ！！！\033[0m\n"
+else
+    echo -e "\033[32m---------->>>  push failed  ！！！\033[0m\n"
+
+echo "push end"
+echo "___________________________________________________"
