c5260a6da1859d303520ca40c7804c6c0ddd62ca
Zhang <zhangyunhai210@gmail.com>
2018-06-09 23:08:25 -0700

    2018-06-09

--- gitpush.sh
+++ gitpush.sh
@@ -7,7 +7,7 @@ echo "add end..."
 git commit -m "${DATE}"
 echo "commit end..."
 git push origin hexo
-if[$? -eq 0] then
+if[$? -eq 0];then
     echo -e "\033[32m---------->>>  push succeed ！！！\033[0m\n"
 else
     echo -e "\033[32m---------->>>  push failed  ！！！\033[0m\n"
